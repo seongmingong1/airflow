65ae5c43c5eb897ae9e6e9e04fb888afad888e85
seongmingong1 <seongmingong14@gmail.com>
2024-10-04 20:20:28 +0200

    shell file test

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,15 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ] ;then
+	echo "You Selected Apple!"
+elif [ $FRUIT == ORANGE ] ;then
+	echo "You Selected Orange!"
+elif [ $FRUIT == GRAPE ] ;then
+	echo "You Selected Grape!"
+else
+	echo "You selected other Fruit!"
+
+fi
+
+
+
+
