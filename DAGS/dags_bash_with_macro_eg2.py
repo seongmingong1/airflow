f35effd75a4b5ad6537d34dcf720deb9735097d9
seongmingong1 <seongmingong14@gmail.com>
2024-10-10 23:42:59 +0200

    macro modified

--- DAGS/dags_bash_with_macro_eg2.py
+++ DAGS/dags_bash_with_macro_eg2.py
@@ -17,3 +17,5 @@ with DAG(
             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds }}'
          },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+    )
+    
